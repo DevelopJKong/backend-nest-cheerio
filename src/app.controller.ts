141b0eb630ff70afa770866f0733f5f4d1d214ca
cafe small house <tbaram10@naver.com>
2023-03-04 00:02:20 +0900

    cheerio 를 사용한 크롤링 완료

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('/cheer')
+  getCheer(): Promise<any> {
+    return this.appService.getCheer();
+  }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,27 @@
 import { Injectable } from '@nestjs/common';
-
+import axios from 'axios';
+import * as cheerio from 'cheerio';
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  async getCheer(): Promise<any> {
+    const { data } = await axios.get('https://www.melon.com/chart/index.htm', {
+      headers: {
+        'User-Agent':
+          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36',
+      },
+    });
+    const $ = cheerio.load(data);
+    $('table > tbody > tr')
+      .find('.ellipsis.rank01')
+      .each((i, e) => {
+        console.log(i);
+        console.log($(e).text().trim());
+      });
+
+    return data;
   }
 }
